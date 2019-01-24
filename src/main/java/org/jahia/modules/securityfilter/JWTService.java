917e24e0a79f3e509889d0e5caec7f56f9ec2852
Sergiy Shyrkov <sshyrkov@jahia.com>
2019-01-24 13:42:29 +0100

    [BACKLOG-9486] Excluded own (embedded) dependencies to Jackson and use the one, provided by DX Jackson OSGi feature. Updated parent version of jahia-modules to 7.2.1.1, where we've introduced Jackson feature (https://jira.jahia.org/browse/QA-9738)

--- src/main/java/org/jahia/modules/securityfilter/JWTService.java
+++ src/main/java/org/jahia/modules/securityfilter/JWTService.java
@@ -45,8 +45,6 @@ package org.jahia.modules.securityfilter;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.jahia.modules.securityfilter.jwt.graphql.generateJWTToken.GraphQLToken;
 
 import javax.jcr.RepositoryException;
 import java.util.Map;
