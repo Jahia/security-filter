d8c8bad36f76291cfba43eb2ae5dfcb5caafa49a
Thomas Draier <tdraier@jahia.com>
2021-06-24 11:50:59 +0200

    Always allow html views

---
+++ src/main/java/org/jahia/modules/securityfilter/core/apply/AlwaysAutoApply.java
@@ -0,0 +1,27 @@
+package org.jahia.modules.securityfilter.core.apply;
+
+import org.jahia.services.modulemanager.util.PropertiesValues;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AlwaysAutoApply implements AutoApply {
+
+    public static AutoApply build(PropertiesValues values) {
+        String value = values.getProperty("always");
+        if (value != null) {
+            return new AlwaysAutoApply();
+        }
+
+        return null;
+    }
+
+    public AlwaysAutoApply() {
+    }
+
+    @Override
+    public boolean shouldApply(HttpServletRequest request) {
+        return true;
+    }
+}
